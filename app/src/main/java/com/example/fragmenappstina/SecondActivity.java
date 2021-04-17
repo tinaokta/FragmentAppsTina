6cc5e3cc727e6cac9cd96ca868d2afe32bb19d78
Tina Okta Riyanti <tina>
2021-04-17 20:34:29 +0700

    sesi kedua

--- app/src/main/java/com/example/fragmenappstina/SecondActivity.java
+++ app/src/main/java/com/example/fragmenappstina/SecondActivity.java
@@ -1,6 +1,28 @@
 package com.example.fragmenappstina;
 
 import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
 
-public class SecondActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+
+        Log.v("CREATING", "SECOND ACTIVITY");
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            finish();
+            return;
+        }
+
+        if (saveInstanceState == null){
+            SecondFragment details = new SecondFragment();
+            getFragmentManager().beginTransaction().add(android.R.id.content, details).commit();
+        }
+    }
 }
--- app/src/main/java/com/example/fragmenappstina/SecondFragment.java
+++ app/src/main/java/com/example/fragmenappstina/SecondFragment.java
@@ -1,4 +1,19 @@
 package com.example.fragmenappstina;
 
-public class SecondFragment {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+//import androidx.fragment.app.Fragment;
+
+public class SecondFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle saveInstanceState){
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
 }
